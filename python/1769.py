711c8bc15c6df2e83b909fcd572c36e9e082eefe
Jennifer <yenjennifer@gmail.com>
2024-03-24 00:32:52 +0800

    Update 1769.py

--- python/1769.py
+++ python/1769.py
@@ -1,16 +1,14 @@
 boxes = "001011"
-dict = {}
-zeroes = []
 ones = []
-index = 0
 ans = []
+index = 0
 for i in boxes:
     if i == '1':
         ones.append(index)
     index+=1
 for j in range(len(boxes)):
     sum = 0
-    for x in ones: #2,4,5
+    for x in ones: 
         sum += abs(x-j)
     ans.append(sum)
 print(ans)
