5559bb05967a8a08fb377a3832643f275c4f78d9
Jennifer <yenjennifer@gmail.com>
2024-03-26 01:20:54 +0800

    [1122]Relative Sort Array

---
+++ python/1122.py
@@ -0,0 +1,20 @@
+arr = [10,11,12]
+
+n = len(arr)
+ans = 0
+# ans = sum(arr)
+temp = sum(arr[0:1])
+for length in range(3, n+1, 2):
+    presum = temp + arr[length-2] + arr[length-1]
+    temp = presum
+    ans += presum
+    for start in range (1,n-length+1):
+        presum = presum - arr[start-1] + arr[start+length-1]
+        ans+=presum
+
+if n%2 !=0:
+    ans+=temp
+else:
+    ans+= temp + arr[n-1]
+
+print(ans)
