e966f475705e65faafa7922cef76ef9d3d048c85
Jennifer <yenjennifer@gmail.com>
2024-04-09 01:22:08 +0800

    Update 20.py

--- python/20.py
+++ python/20.py
@@ -1,17 +1,19 @@
 # s = "({[]})"
 s = "()[]{}"
 
-stack = []
-# closeToOpen = {']':'[', '{':'}' , '(':')'}
-# for c in s:
-#     if c in closeToOpen:
-#         if stack and stack[-1] == closeToOpen[c]:
-#             stack.pop()
-#         else:
-#             return False
-#     else: 
-#         stack.append(c)
-if stack:
-    print(True) 
-else: 
-    print(False) 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {']':'[', '}':'{' , ')':'('}
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else: 
+                stack.append(c)
+        if not stack:
+            return True
+        else: 
+            return False
